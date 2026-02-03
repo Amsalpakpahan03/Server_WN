e80fea43cc149f2a88ac89ab717af40e84c89b0f
Amsalpakpahan03 <107837478+Amsalpakpahan03@users.noreply.github.com>
2026-02-03 21:58:05 +0700

    Update OrderService.js

--- services/OrderService.js
+++ services/OrderService.js
@@ -1,9 +1,36 @@
 const Order = require("../models/Order");
 
 // ================= CREATE ORDER =================
+// exports.createOrder = async ({ tableNumber, items, totalPrice }) => {
+//   if (!tableNumber) {
+//     throw new Error("TABLE_INVALID");
+//   }
+
+//   const orderData = {
+//     tableNumber,
+//     items,
+//     totalPrice,
+//     status: "pending",
+//   };
+
+//   const newOrder = new Order(orderData);
+//   return await newOrder.save();
+// };
+// services/OrderService.js
+
 exports.createOrder = async ({ tableNumber, items, totalPrice }) => {
   if (!tableNumber) {
     throw new Error("TABLE_INVALID");
+  }
+
+  // PROTEKSI: Cek apakah meja ini punya order yang masih diproses
+  const activeOrder = await Order.findOne({
+    tableNumber,
+    status: "pending",
+  });
+
+  if (activeOrder) {
+    throw new Error("TABLE_OCCUPIED"); // Lempar error jika meja masih dipakai
   }
 
   const orderData = {
@@ -16,16 +43,13 @@ exports.createOrder = async ({ tableNumber, items, totalPrice }) => {
   const newOrder = new Order(orderData);
   return await newOrder.save();
 };
-
 // ================= GET ALL ORDERS =================
 // exports.getAllOrders = async () => {
 //   return await Order.find().sort({ createdAt: -1 });
 // };
 exports.getAllOrders = async () => {
-  return await Order.find()
-    .sort({ createdAt: 1, _id: 1 });
+  return await Order.find().sort({ createdAt: 1, _id: 1 });
 };
-
 
 // ================= GET ORDER BY ID =================
 exports.getOrderById = async (id) => {
@@ -34,9 +58,5 @@ exports.getOrderById = async (id) => {
 
 // ================= UPDATE STATUS =================
 exports.updateStatus = async (id, status) => {
-  return await Order.findByIdAndUpdate(
-    id,
-    { status },
-    { new: true }
-  );
+  return await Order.findByIdAndUpdate(id, { status }, { new: true });
 };
